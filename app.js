cd596ab1dd665810df99ce64f7a2d58a0f58e17b
Ellen Liu <Huailun@users.noreply.github.com>
2018-05-08 00:26:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,66 +7,96 @@ let skinColor = document.querySelector('#skin-color')
 let eyeColor  = document.querySelector('#eye-color')
 let gender    = document.querySelector('#gender')
 let birthYear = document.querySelector('#birth-year')
-let home      = document.querySelector('#home');
+let movies    = document.querySelector('#movieList')
+let resident  = document.querySelector('#resident')
+let home      = document.querySelector('#home')
 
-function getInfo() {
-    for (var i = 1; i <= 88; i++ ){
-        let apiUrl = 'https://swapi.co/api/people/' + i;
-        axios.get(apiUrl).then(function(response){
-            updateInfo(response.data) 
-            console.log(response.data)         
-        })
-    }  
-}
 
-function getHome() {
-    for (let j = 1; j <= 60; j++ ){
-        let apiUrlPlanet = 'https://swapi.co/api/planets/' + j;
-        axios.get(apiUrlPlanet).then(function(response){
-            updateInfo(response.data)     
-        })
-    }  
-}
+document.addEventListener('DOMContentLoaded', bindButton);
 
-getInfo();
-getHome();
+function bindButton() {
 
-function updateInfo(data) {
+  $("#submit").click(function(e){
+    var star = document.getElementById('star');
+  for (var j = 1; j < 10; j++) {
+  (function(i) {
+  var f = i;
+  var req = new XMLHttpRequest();
+  var URLhost = 'https://swapi.co/api/people/?page=' + f;
+  req.open('GET', URLhost, true);
+  req.addEventListener('load', function() {
+    if (req.status >= 200 && req.status < 400) {
+      var response = JSON.parse(req.responseText);
+      console.log(response);
+      for (var k = 0; k < response.results.length; k++) {
+        (function(y) {
+          var val = $("#input").val();
+          if ( val.toLowerCase() === response.results[y].name.toLowerCase()) {
+            name.innerText = response.results[y].name;
+            console.log(response.results[y].name);
+            height.innerText = response.results[y].height;
+            mass.innerText = response.results[y].mass;
+            hairColor.innerText = response.results[y].hair_color;
+            skinColor.innerText = response.results[y].skin_color;
+            eyeColor.innerText = response.results[y].eye_color;
+            birthYear.innerText = response.results[y].birth_year; 
+            gender.innerText = response.results[y].gender;
+            var homeReq = new XMLHttpRequest();
+            var planetURL = response.results[y].homeworld.toString();
+            homeReq.open('GET', planetURL, true);
+            homeReq.addEventListener('load', function () {
+              if (homeReq.status >= 200 && homeReq.status < 400) {
+                var planetResponse = JSON.parse(homeReq.responseText);
+                console.log(planetResponse);
+                home.innerText = planetResponse.name;
+              } else {
+                console.log('Error in network request: ' + req.statusText);
+              }});
+              homeReq.send(null);
+              event.preventDefault();
 
-    $("#submit").click(function(e){
-        e.preventDefault();
-            
-            var val=$("#input").val();
-            if( val.toLowerCase() === data.name.toLowerCase() ) {
-                name.innerText = data.name;
-                height.innerText = data.height;
-                mass.innerText = data.mass;
-                hairColor.innerText = data.hair_color;
-                skinColor.innerText = data.skin_color;
-                eyeColor.innerText = data.eye_color;
-                birthYear.innerText = data.birth_year; 
-                gender.innerText = data.gender; 
-            } else {
-                $("#list-names").css("visibility","visible");
-            }                    
-    });
+            if (response.results[y].films.length > 0) {
+              var movieList = document.createElement('ol');
+              for (var e = 0; e < response.results[y].films.length; e++) {
+                (function(x) {
+                  var newURLhost = response.results[y].films[x];
+                  var newReq = new XMLHttpRequest();
+                  newReq.open('GET', newURLhost, true);
+                  newReq.addEventListener('load', function(){
+                    if(newReq.status >= 200 && newReq.status < 400){
+                      var newResponse = JSON.parse(newReq.responseText);
+                      var movie = document.createElement('li');
+                      movie.textContent = newResponse.title + ' ' + newResponse.release_date;
+                      movieList.appendChild(movie);
+                      console.log(movieList);
+                      var list = document.getElementById('movieList');     // Get the target element
+                       list.innerHTML = "";                            // Remove previous content
+                       list.appendChild(movieList); // Append your generated UL 
+
+                    } else {
+                      console.log("Error in network request: " + newReq.statusText);
+                    }});
+                  newReq.send(null);
+                  event.preventDefault();
+                })(e);
+              }
+          }         
+          }
+    })(k);
+    }
     
-        $("#button").click(function(e){
-            e.preventDefault();
-            let person = [];
-            person = data.residents;
-            if (typeof person !== undefined) {
-                home.innerText = data.name;
-            }
-           
-           
-           
-               
-        });
-    
+    } else {
+      console.log('Error in network request: ' + req.statusText);
+    }});
+    req.send(null);
+    //event.preventDefault();
+    })(j)
+    }
+  });
 }
 
 
 
+  
 
 
